92bfea368f56db5d30114c1cf4f9bdf7bfb542f0
Vic564 <victor.a.persson@gmail.com>
2020-02-05 11:19:23 +0100

    testar ett schema

--- db/db.js
+++ db/db.js
@@ -1,1 +1,19 @@
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/database', {useNewUrlParser: true});
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  //name: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  //location: String,
+  //created_at: Date,
+  //updated_at: Date
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
