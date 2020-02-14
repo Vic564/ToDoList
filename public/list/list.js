628efc63b942b0c64b66d0e5f798d8d4d0f45181
Vic564 <victor.a.persson@gmail.com>
2020-02-14 23:52:08 +0100

    sista commit css och finlir

--- public/list/list.js
+++ public/list/list.js
@@ -13,19 +13,21 @@ const updatePage = (sort = "prio") => {
     window.location.href = url;
 }
 
-const saveTask = (button) => {
+const saveTask = () => {
     const task = {
         note: document.querySelector("#task_note").value,
         prio: document.querySelector("#task_prio").value
     };
-    console.log(task)
-    console.log('testar console log')
-
-    postTask(task)
-    .then(response => {
-        updatePage();
-    })
-    .catch(error => alert(`ERROR ${error}`));
+    if (task.note.length > 0) {
+        postTask(task)
+        .then(() => {
+            updatePage();
+        })
+        .catch(error => alert(`ERROR ${error}`));
+    }
+    else {
+        alert("You have to write a task!");
+    }
 }
 
 //Funktion som skickar objektet till servern och resolvar eller rejectar beroende på om den hittar en matchning i db
@@ -57,7 +59,7 @@ const postTask = (task) => {
 
 const removeTask = (button) => {
     deleteTask(button.parentNode.parentNode.id.replace("task_", ""))
-    .then(response => {
+    .then(() => {
         updatePage();
     })
     .catch(error => alert(`ERROR ${error}`));
@@ -120,7 +122,7 @@ const completeTask = (button) => {
         id: button.parentNode.parentNode.id.replace("task_", "")
     };
     putStatus(task)
-    .then(response => {
+    .then(() => {
         updatePage();
     })
     .catch(error => alert(`ERROR ${error}`));
@@ -152,55 +154,72 @@ const putStatus = (task) => {
     });
 }
 
+const toggleOptions  = (taskHeading) => {
+    taskHeading.parentNode.parentNode.childNodes[3].classList.toggle("options-visible");
+    taskHeading.classList.toggle("task-heading-selected")
+}
+
 const toggleEditForm = (button) => {
     const taskid = button.parentNode.parentNode.id.replace("task_", "");
+    
+    button.parentNode.classList.toggle("options-visible");
+
+    const currentPrio = button.parentNode.parentNode.childNodes[1].childNodes[1].innerHTML;
+    const currentNote = button.parentNode.parentNode.childNodes[1].childNodes[3].innerHTML;
+
     button.parentNode.parentNode.childNodes[1].childNodes[1].remove();
     button.parentNode.parentNode.childNodes[1].childNodes[2].remove();
+
+    console.log(currentNote);
 
     let inputPrio = document.createElement("input");
     inputPrio.id = "m_task_prio_" + taskid;
     inputPrio.setAttribute("type", "number");
+    inputPrio.value = currentPrio;
+    inputPrio.setAttribute("min", "1");
+    inputPrio.setAttribute("max", document.querySelector("#task_prio").value - 1);
     inputPrio.setAttribute("class", "task-prio-active");
     button.parentNode.parentNode.childNodes[1].insertBefore(inputPrio, button.parentNode.parentNode.childNodes[1].childNodes[1]);
 
     let inputTask = document.createElement("input");
     inputTask.id = "m_task_note_" + taskid;
     inputTask.setAttribute("type", "text");
+    inputTask.value = currentNote;
     inputTask.setAttribute("class", "task-note-active");
     button.parentNode.parentNode.childNodes[1].insertBefore(inputTask, button.parentNode.parentNode.childNodes[1].childNodes[3]);
 
     let saveButton = document.createElement("button");
     saveButton.setAttribute("type", "button");
     saveButton.innerText = "SAVE";
+    saveButton.classList.add("text-green")
     saveButton.addEventListener("click", () => {
         const task = {
             id: taskid,
             note: document.querySelector(`#${inputTask.id}`).value,
             prio: document.querySelector(`#${inputPrio.id}`).value
         };
-        putTask(task)
-        .then(response => {
-            updatePage();
-        })
-        .catch(error => alert(`ERROR ${error}`));
+        if (task.note.lenth > 0) { 
+            putTask(task)
+            .then(response => {
+                updatePage();
+            })
+            .catch(error => alert(`ERROR ${error}`));
+        }
+        else {
+            alert("You have to write a task!");
+        }
     });
     button.parentNode.parentNode.appendChild(saveButton);
+
+    let cancelButton = document.createElement("button");
+    cancelButton.setAttribute("type", "button");
+    cancelButton.innerText = "CANCEL";
+    cancelButton.classList.add("text-red")
+    cancelButton.addEventListener("click", () => updatePage());
+    button.parentNode.parentNode.appendChild(cancelButton);
 }
 
-const editTask = (button) => {
-    const notePrefix = "m_task_note_";
-    const prioPrefix = "m_task_prio_";
-    const taskid = button.parentNode.parentNode.id.replace("task_", "");
-    const task = {
-        id: button.parentNode.parentNode.id,
-        note: document.querySelector(`#${notePrefix}${taskid}`).value,
-        prio: document.querySelector(`#${prioPrefix}${taskid}`).value
-    };
-    putTask(task)
-    .then(response => {
-        updatePage();
-    })
-    .catch(error => alert(`ERROR ${error}`));
+const toggleSaveForm = (button) => {
+    document.querySelector('#save_form').classList.toggle("save-form-show");
+    button.classList.toggle("hidden");
 }
-
-const toggleSaveForm = () => document.querySelector('#save_form').classList.toggle("save-form-show");
