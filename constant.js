a0cb0f73379750affe6a59c2089fbb74a56b8494
Vic564 <victor.a.persson@gmail.com>
2020-03-27 21:16:08 +0100

    förbättrat databasanslutning med .env

--- constant.js
+++ constant.js
@@ -14,10 +14,19 @@ const ROUTE = {
     wildcard: '/*'
 }
 
+const mongodb = process.env.MONGODB || 'mongodb';
+const dbUser = process.env.DBUSER || 'test';
+const dbPassword = process.env.DBPASSWORD || 'test';
+const dbHostname = process.env.DBHOSTNAME || 'localhost';
+const dbName = process.env.DBNAME || 'todolistapp';
+const dbQuery = process.env.DBQUERY || '';
+const dbAuth = (process.env.DBAUTH == 'true') ? true : false;
+
 const CONFIG = {
     PORT: process.env.PORT || 8080,
     MONGO: {
-        connection: `mongodb://${process.env.DBHOSTNAME || 'localhost'}/${process.env.DBNAME || 'todolistapp'}`,
+        connection: dbAuth ? `${mongodb}://${dbUser}:${dbPassword}@${dbHostname}/${dbName}?${dbQuery}`
+        : `${mongodb}://${dbHostname}/${dbName}?${dbQuery}`,
         options: {
             useNewUrlParser: true,
             useUnifiedTopology: true,
