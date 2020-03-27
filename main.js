d13f1a2679c6ee988a0fdc52d3c8a3ac9d40cbf7
Vic564 <victor.a.persson@gmail.com>
2020-03-27 20:57:30 +0100

    oneline arrow function

--- main.js
+++ main.js
@@ -4,7 +4,5 @@ const {CONFIG} = require('./constant');
 const {app} = require('./app');
 
 mongoose.connect(CONFIG.MONGO.connection, CONFIG.MONGO.options)
-    .then(() => {
-        app.listen(CONFIG.PORT, () => console.log(`SERVER LISTENING ON PORT: ${CONFIG.PORT}`));
-    })
+    .then(() => app.listen(CONFIG.PORT, () => console.log(`SERVER LISTENING ON PORT: ${CONFIG.PORT}`)))
     .catch(error => console.error(error));
