211b3ef6d4a0d83ac87a5f5cba632ff98ad0d881
Vic564 <victor.a.persson@gmail.com>
2020-03-28 12:00:08 +0100

    async create_example.js

--- create_example.js
+++ create_example.js
@@ -4,93 +4,89 @@ const User = require('./model/user');
 const Task = require('./model/task');
 const {CONFIG} = require('./constant');
 
-const deleteUser = () => {
-    return new Promise((resolve, reject) => {
-        User.deleteOne({username: CONFIG.EXAMPLE.username}, error => {
+const deleteUser = async () => {
+    return new Promise(async (resolve, reject) => {
+        await User.deleteOne({username: CONFIG.EXAMPLE.username}, async error => {
             if (error) {
                 reject(error);
-            } else {
-                console.log(`EXAMPLE USER ${CONFIG.EXAMPLE.username} DELETED`);
-                resolve();
             }
+            console.log(`EXAMPLE USER ${CONFIG.EXAMPLE.username} DELETED`);
+            resolve();
         });
     });
 }
 
-const deleteTasks = () => {
-    return new Promise((resolve, reject) => {
-        User.findOne({username: CONFIG.EXAMPLE.username}, (error, user) => {
+const deleteTasks = async () => {
+    return new Promise(async (resolve, reject) => {
+        await User.findOne({username: CONFIG.EXAMPLE.username}, async (error, user) => {
             if (error) {
                 reject(error);
             }
-            else {
-                if (user) {
-                    Task.deleteMany({userID: user._id}, error => {
-                        if (error) {
-                            reject(error);
-                        }
-                    });
-                }
+            else if (user) {
+                await Task.deleteMany({userID: user._id}, async error => {
+                    if (error) {
+                        reject(error);
+                    }
+                });
                 console.log(`EXAMPLE USER ${CONFIG.EXAMPLE.username} TASKS DELETED`);
-                resolve();
             }
+            resolve();
         });
     });
 }
 
-const createUser = () => {
-    return new Promise((resolve, reject) => {
+const saveModel = async (model) => {
+    return new Promise(async (resolve, reject) => {
+        await model.save(async error => {
+            if (error) reject(error)
+            resolve()
+        });
+    });
+}
+
+const createUser = async () => {
+    return new Promise(async (resolve, reject) => {
         const exampleUser = new User({
             username: CONFIG.EXAMPLE.username,
             password: CONFIG.EXAMPLE.password
         });
-        exampleUser.save(error => {
-            if (error) {
-                reject(error);
-            }
-            else {
-                console.log(`EXAMPLE USER ${CONFIG.EXAMPLE.username} SAVED`);
-                resolve();
-            }
-        });
+        await saveModel(exampleUser).catch(error => reject(error));
+        console.log(`EXAMPLE USER ${CONFIG.EXAMPLE.username} SAVED`);
+        resolve();
     });
 }
 
-const createTasks = () => {
-    return new Promise((resolve, reject) => {
-        User.findOne({username: CONFIG.EXAMPLE.username}, (error, user) => {
+const createTasks = async () => {
+    return new Promise(async (resolve, reject) => {
+        await User.findOne({username: CONFIG.EXAMPLE.username}, async (error, user) => {
             if (error) {
                 reject(error);
             }
-            else {
+            else if (user) {
                 const taskList = ['eat', 'sleep', 'repeat'];
-                for (const [i, note] of taskList.entries()) {
+                for await (const [i, note] of taskList.entries()) {
                     const task = new Task({
                         userID: user._id,
                         note: note,
                         status: false,
                         prio: i + 1
                     });
-                    task.save(error => {
-                        if (error) {
-                            reject(error);
-                        }
-                    });
+                    await saveModel(task).catch(error => reject(error));
                 }
                 console.log(`EXAMPLE USER ${CONFIG.EXAMPLE.username} TASKS SAVED`);
-                resolve();
             }
+            resolve();
         });
     });
 }
 
 mongoose.connect(CONFIG.MONGO.connection, CONFIG.MONGO.options)
-    .then(() => deleteTasks())
-    .then(() => deleteUser())
-    .then(() => createUser())
-    .then(() => createTasks())
-    .then(() => mongoose.connection.close())
-    .catch(err => {
-        console.error(err);
-        mongoose.connection.close();
-    });
+    .then(async () => await deleteTasks())
+    .then(async () => await deleteUser())
+    .then(async () => await createUser())
+    .then(async () => await createTasks())
+    .then(async () => await Task.find({}))
+    .then(async list => console.log(list))
+    .catch(err => console.error(err))
+    .then(async () => await mongoose.connection.close())
+    .catch(err => console.error(err));
